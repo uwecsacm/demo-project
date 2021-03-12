4676096845b34119ad07a7b09b0e35235df6f411
SACM Demo <sacm@uwec.edu>
2021-03-11 18:47:50 -0600

    Added name prompt

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = in.nextLine();
+        System.out.println("Your name is " + name);
     }
 }
